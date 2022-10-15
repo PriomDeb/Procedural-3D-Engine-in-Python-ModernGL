d0aabee395f3bf09056f86d07f97f2d3f43cc04c
PriomDeb <87948186+PriomDeb@users.noreply.github.com>
2022-10-15 16:39:11 +0600

    A basic sphere is rendered

--- programs/fragment.glsl
+++ programs/fragment.glsl
@@ -1,11 +1,45 @@
 #version 330 core
-
 layout(location = 0) out vec4 fragColor;
 
 uniform vec2 u_resolution;
 
+const float FOV = 1.0;
+const int MAX_STEPS = 256;
+const float MAX_DIST = 500;
+const float EPSILON = 0.001;
+
+vec2 map(vec3 p){
+    // Sphere
+    float sphereDist = length(p) - 1.0;
+    float sphereID = 1.0;
+    vec2 sphere = vec2(sphereDist, sphereID);
+    // Result
+    vec2 res = sphere;
+    return res;
+}
+
+vec2 rayMarch(vec3 ro, vec3 rd){
+    vec2 hit, object;
+    for (int i = 0; i < MAX_STEPS; i++){
+        vec3 p = ro + object.x * rd;
+        hit = map(p);
+        object.x += hit.x;
+        object.y = hit.y;
+        if (abs(hit.x) < EPSILON || object.x > MAX_DIST) break;
+    }
+    return object;
+}
+
 void render(inout vec3 col, in vec2 uv){
-    col.rg += uv;
+//    col.rg += uv;
+    vec3 ro = vec3(0.0, 0.0, -3.0);
+    vec3 rd = normalize(vec3(uv, FOV));
+
+    vec2 object = rayMarch(ro, rd);
+
+    if (object.x < MAX_DIST){
+        col += 3.0 / object.x;
+    }
 }
 
 void main() {
@@ -16,3 +50,4 @@ void main() {
 
     fragColor = vec4(col, 1.0);
 }
+
